3ab82e105ec629223f8a291ae551d1698862cfe3
Dimas Roger <dimasroger89@gmail.com>
2021-05-06 01:19:25 +0700

    fixing ui ux beranda

--- components/BerandaComponent.vue
+++ components/BerandaComponent.vue
@@ -30,6 +30,8 @@
             <v-btn
               depressed
               rounded
+              target="_blank"
+              href="https://drive.google.com/file/d/1qXcYpTClJQH2Y2rNecfm8p14xl3O6u3T/view?usp=sharing"
               class="focus:tw-outline-none tw-text-white tw-tracking-wide tw-normal-case tw-font-bold tw-px-6 tw-bg-gradient-to-r tw-from-primary-1 tw-to-primary"
               >Get CV</v-btn
             >
@@ -111,6 +113,8 @@
               depressed
               rounded
               small
+              target="_blank"
+              href="https://drive.google.com/file/d/1qXcYpTClJQH2Y2rNecfm8p14xl3O6u3T/view?usp=sharing"
               class="focus:tw-outline-none tw-text-white tw-tracking-wide tw-normal-case tw-px-6 tw-bg-gradient-to-r tw-from-primary-1 tw-to-primary"
               >Get CV</v-btn
             >
@@ -134,8 +138,12 @@
             >
               <v-img
                 contain
-                :height="item.height"
-                :width="item.width"
+                :height="
+                  $vuetify.breakpoint.smAndUp ? item.height : item.height + 4
+                "
+                :width="
+                  $vuetify.breakpoint.smAndUp ? item.width : item.width + 4
+                "
                 :src="item.src"
                 class="tw-mr-5 tw-cursor-pointer"
               ></v-img>
@@ -154,35 +162,35 @@ export default {
       socialMedia: [
         {
           text: 'instagram',
-          height: 26,
-          width: 26,
+          height: 24,
+          width: 24,
           href: 'https://www.instagram.com/rogersovichhh/?hl=id',
           src:
-            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-instagram_1_gejV_R2ZIEE3.png',
+            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-instagram-150_pvAwfqy8l.png',
         },
         {
           text: 'email',
-          height: 22,
-          width: 22,
+          height: 26,
+          width: 26,
           href: 'mailto:dimasroger89@gmail.com',
           src:
-            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-mail_1_xNz44cbwv.png',
+            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-mail-150_vRCz0jWKN.png',
         },
         {
           text: 'linkedin',
-          height: 22,
-          width: 22,
+          height: 26,
+          width: 26,
           href: 'https://www.linkedin.com/in/dimas-roger-9b93491a1/',
           src:
-            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-linkedin_1_ZocfHgbrP.png',
+            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-linkedin-150_lRDzLzGaU.png',
         },
         {
           text: 'secret',
-          height: 30,
-          width: 30,
+          height: 25,
+          width: 25,
           href: 'https://github.com/rogersovich',
           src:
-            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-pentagram-devil-100_NxUDCr-QN.png',
+            'https://ik.imagekit.io/1akf8cdsyg/cv-rogersovich/Assets/icons8-github-150_jwXCRvu0C.png',
         },
       ],
     }
